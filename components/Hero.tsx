5f83b547d5f22aa6b633b9e76e6d3b48866e2aff
Emmanuel Makoye <makoye224@gmail.com>
2023-11-08 17:01:06 -0500

    added shadow to banner image and configured studio

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -32,10 +32,10 @@ const Hero = () => {
       <div className="hero__image-container">
         <div className="hero__image">
           <Image
-            src="/hel-bn.png"
+            src="/bannerr.png"
             alt="hero"
             fill
-            className="object-contain shadow-black "
+            className="object-contain"
           />
         </div>
 
